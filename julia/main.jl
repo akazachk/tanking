e21db0f1668e4180589190fb4ba4df5c6f828284
Aleksandr Kazachkov <None>
2018-10-28 00:07:18 -0400

    Added plots for real NBA seasons

--- julia/main.jl
+++ julia/main.jl
@@ -1,5 +1,6 @@
 # Needs Plots, DelimitedFiles, GR
 using Plots
+using StatPlots
 using DelimitedFiles
 using LaTeXStrings
 using Printf
@@ -8,6 +9,13 @@ include("parse.jl")
 
 ## When the (draft) ranking will be set as fraction of number games
 set_ranking = [4//8; 5//8; 6//8; 7//8; 1]
+num_rankings = length(set_ranking)
+shape = [:vline, :utriangle, :rect, :x, :circle]
+col = ["red", "orange", "green", "blue", "black"]
+#color_for_cutoff_point = ["c" "b" "m" "r" "k"]
+
+## For plotting
+ext = ".svg"
 
 function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=true, results_dir = "../results")
 	## Variables that need to be set
@@ -15,12 +23,6 @@ function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=t
 	num_rounds = 3 # a round consists of each team playing each other team
 	num_steps = 20 # discretization of [0,1] for tanking probability
 	gamma = 0.75 # probability a better-ranked team wins over a worse-ranked team
-
-	## When the (draft) ranking will be set as fraction of number games
-	num_rankings = length(set_ranking)
-	col = ["red", "orange", "green", "blue", "black"]
-	shape = [:vline, :utriangle, :rect, :x, :circle]
-	#color_for_cutoff_point = ["c" "b" "m" "r" "k"]
 	## end variables that need to be set
 
 	## Set constants
@@ -58,7 +60,6 @@ function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=t
 		upscale = 1 # upscaling in resolution
 		fntsm = Plots.font("sans-serif", 8.0 * upscale)
 		fntlg = Plots.font("sans-serif", 12.0 * upscale)
-		ext = ".svg"
 		#default(titlefont=fntlg, guidefont=fntlg, tickfont=fntsm, legendfont=fntsm)
 		default(size=(600*upscale,400*upscale)) # plot canvas size
 
@@ -68,7 +69,7 @@ function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=t
 		maxy = Int(floor(findmax(avg_kend)[1]))
 
 		fig = Plots.plot(show=false,
-										#title=L"\mbox{Fidelity of ranking by cutoff and tanking probability}",
+										#title=L"\mbox{Fidelity of ranking by breapoint and tanking probability}",
 										title=L"\mbox{Accuracy of ranking}",
 										#xlab=L"\mbox{Percentage of tanking teams}", 
 										xlab=L"\mbox{Probability of tanking}", 
@@ -99,7 +100,7 @@ function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=t
 		end
 		savefig(fig, string(results_dir,"/avg_kend",ext));
 
-		## Plot avg_games_tanked (# games tanked by draft ranking cutoff)
+		## Plot avg_games_tanked (# games tanked by draft ranking breakpoint)
 		print("Plotting avg_games_tanked: average number of games tanked\n")
 		miny = Int(ceil(findmin(avg_games_tanked)[1]))
 		maxy = Int(floor(findmax(avg_games_tanked)[1]))
@@ -189,32 +190,121 @@ function main_simulate(do_simulation = true, num_repeats = 100000, do_plotting=t
 	return
 end # main_simulate
 
-function main_parse(data_dir="../data", results_dir="../results")
-	num_eliminated_at_cutoff1314, num_games_tanked_at_cutoff1314, stats1314, critical_game1314 = parseNBASeason("games1314.csv", set_ranking, data_dir)
-	num_eliminated_at_cutoff1415, num_games_tanked_at_cutoff1415, stats1415, critical_game1415 = parseNBASeason("games1415.csv", set_ranking, data_dir)
-	num_eliminated_at_cutoff1516, num_games_tanked_at_cutoff1516, stats1516, critical_game1516 = parseNBASeason("games1516.csv", set_ranking, data_dir)
-	num_eliminated_at_cutoff1617, num_games_tanked_at_cutoff1617, stats1617, critical_game1617 = parseNBASeason("games1617.csv", set_ranking, data_dir)
-	num_eliminated_at_cutoff1718, num_games_tanked_at_cutoff1718, stats1718, critical_game1718 = parseNBASeason("games1718.csv", set_ranking, data_dir)
+function main_parse(do_plotting=true, data_dir="../data", results_dir="../results")
+	num_teams_eliminated_1314, num_games_tanked_1314, stats1314, critical_game1314 = parseNBASeason("games1314.csv", set_ranking, data_dir)
+	num_teams_eliminated_1415, num_games_tanked_1415, stats1415, critical_game1415 = parseNBASeason("games1415.csv", set_ranking, data_dir)
+	num_teams_eliminated_1516, num_games_tanked_1516, stats1516, critical_game1516 = parseNBASeason("games1516.csv", set_ranking, data_dir)
+	num_teams_eliminated_1617, num_games_tanked_1617, stats1617, critical_game1617 = parseNBASeason("games1617.csv", set_ranking, data_dir)
+	num_teams_eliminated_1718, num_games_tanked_1718, stats1718, critical_game1718 = parseNBASeason("games1718.csv", set_ranking, data_dir)
 
 	print("Year 2013-2014\n")
-	print("num teams eliminated: ",num_eliminated_at_cutoff1314,"\n")
-	print("num possible games tanked: ",num_games_tanked_at_cutoff1314,"\n")
+	print("num teams eliminated: ",num_teams_eliminated_1314,"\n")
+	print("num possible games tanked: ",num_games_tanked_1314,"\n")
 
 	print("\nYear 2014-2015\n")
-	print("num teams eliminated: ",num_eliminated_at_cutoff1415,"\n")
-	print("num possible games tanked: ",num_games_tanked_at_cutoff1415,"\n")
+	print("num teams eliminated: ",num_teams_eliminated_1415,"\n")
+	print("num possible games tanked: ",num_games_tanked_1415,"\n")
 
 	print("\nYear 2015-2016\n")
-	print("num teams eliminated: ",num_eliminated_at_cutoff1516,"\n")
-	print("num possible games tanked: ",num_games_tanked_at_cutoff1516,"\n")
+	print("num teams eliminated: ",num_teams_eliminated_1516,"\n")
+	print("num possible games tanked: ",num_games_tanked_1516,"\n")
 
 	print("\nYear 2016-2017\n")
-	print("num teams eliminated: ",num_eliminated_at_cutoff1617,"\n")
-	print("num possible games tanked: ",num_games_tanked_at_cutoff1617,"\n")
+	print("num teams eliminated: ",num_teams_eliminated_1617,"\n")
+	print("num possible games tanked: ",num_games_tanked_1617,"\n")
 
 	print("\nYear 2017-2018\n")
-	print("num teams eliminated: ",num_eliminated_at_cutoff1718,"\n")
-	print("num possible games tanked: ",num_games_tanked_at_cutoff1718,"\n")
+	print("num teams eliminated: ",num_teams_eliminated_1718,"\n")
+	print("num possible games tanked: ",num_games_tanked_1718,"\n")
+
+	if (do_plotting)
+		ind = [3,4,5]
+		num_years=5
+
+		## Plot # games tanked
+		print("Plotting num_games_tanked: number of games (possibly) tanked by the breakpoint mark\n")
+		#num_games_tanked = zeros(Int, num_years, length(set_ranking))
+		#num_games_tanked[1,:] = num_games_tanked_1314
+		#num_games_tanked[2,:] = num_games_tanked_1415
+		#num_games_tanked[3,:] = num_games_tanked_1516
+		#num_games_tanked[4,:] = num_games_tanked_1617
+		#num_games_tanked[5,:] = num_games_tanked_1718
+		num_games_tanked = hcat(num_games_tanked_1314, num_games_tanked_1415, num_games_tanked_1516, num_games_tanked_1617, num_games_tanked_1718)
+		num_games_tanked = num_games_tanked'
+		#print(num_games_tanked,"\n")
+
+		num_games_tanked_stacked = zeros(Int, num_years, length(set_ranking))
+		num_games_tanked_stacked[:,1] = num_games_tanked[:,1]
+		num_games_tanked_stacked[:,2] = num_games_tanked[:,2] - num_games_tanked[:,1]
+		num_games_tanked_stacked[:,3] = num_games_tanked[:,3] - num_games_tanked[:,2]
+		num_games_tanked_stacked[:,4] = num_games_tanked[:,4] - num_games_tanked[:,3]
+		num_games_tanked_stacked[:,5] = num_games_tanked[:,5] - num_games_tanked[:,4]
+
+		miny = Int(ceil(findmin(num_games_tanked)[1]))
+		maxy = Int(floor(findmax(num_games_tanked)[1]))
+		inc = (maxy - miny) / 5
+		ctg = repeat(vcat([latexstring(numerator(set_ranking[r]),"/",denominator(set_ranking[r]), "\\mbox{ through season}") for r in ind if r < length(set_ranking)], L"\mbox{end of season}"), inner=num_years)
+		fig = groupedbar(num_games_tanked_stacked[:,ind], 
+										xticks=(Array(1:num_years),["\$13-14\$","\$14-15\$","\$15-16\$","\$16-17\$","\$17-18\$"]),
+										yticks=(Array(0:50:maxy),[@sprintf("\$%d\$", i) for i in 0:50:maxy]),
+										group=ctg,
+										lw=0,
+										#bar_position=:dodge,
+										bar_position=:stack,
+										title=L"\mbox{Number of games that could be tanked}",
+										xlab=L"\mbox{Season}", 
+										ylab=L"\mbox{Number of possibly tanked games}",
+										legend=:best,
+										legendfont=6,
+										legendtitle=L"\mbox{Draft ranking breakpoint}",
+										titlefont=12,
+										tickfont=8,
+										grid=false,
+										show=false)
+		savefig(fig, string(results_dir,"/nba_num_games_tanked",ext));
+
+		# Print number of teams eliminated
+		print("Plotting num_teams eliminated: number of eliminated teams by the breakpoint mark\n")
+		#num_teams_eliminated = zeros(Int, num_years, length(set_ranking))
+		#num_teams_eliminated[1,:] = num_teams_eliminated_1314
+		#num_teams_eliminated[2,:] = num_teams_eliminated_1415
+		#num_teams_eliminated[3,:] = num_teams_eliminated_1516
+		#num_teams_eliminated[4,:] = num_teams_eliminated_1617
+		#num_teams_eliminated[5,:] = num_teams_eliminated_1718
+		num_teams_eliminated = hcat(num_teams_eliminated_1314, num_teams_eliminated_1415, num_teams_eliminated_1516, num_teams_eliminated_1617, num_teams_eliminated_1718)
+		num_teams_eliminated = num_teams_eliminated'
+		#print(num_teams_eliminated,"\n")
+
+		num_teams_eliminated_stacked = zeros(Int, num_years, length(set_ranking))
+		num_teams_eliminated_stacked[:,1] = num_teams_eliminated[:,1]
+		num_teams_eliminated_stacked[:,2] = num_teams_eliminated[:,2] - num_teams_eliminated[:,1]
+		num_teams_eliminated_stacked[:,3] = num_teams_eliminated[:,3] - num_teams_eliminated[:,2]
+		num_teams_eliminated_stacked[:,4] = num_teams_eliminated[:,4] - num_teams_eliminated[:,3]
+		num_teams_eliminated_stacked[:,5] = num_teams_eliminated[:,5] - num_teams_eliminated[:,4]
+
+		miny = Int(ceil(findmin(num_teams_eliminated)[1]))
+		maxy = Int(floor(findmax(num_teams_eliminated)[1]))
+		inc = (maxy - miny) / 5
+		ctg = repeat(vcat([latexstring(numerator(set_ranking[r]),"/",denominator(set_ranking[r]), "\\mbox{ through season}") for r in ind if r < length(set_ranking)], L"\mbox{end of season}"), inner=num_years)
+		fig = groupedbar(num_teams_eliminated[:,ind], 
+										xticks=(Array(1:num_years),["\$13-14\$","\$14-15\$","\$15-16\$","\$16-17\$","\$17-18\$"]),
+										#yticks=(Array(miny:inc:maxy),[@sprintf("\$%d\$", i) for i in miny:inc:maxy]),
+										group=ctg,
+										lw=0,
+										#bar_position=:dodge,
+										bar_position=:stack,
+										title=L"\mbox{Number of teams eliminated by breakpoint mark}",
+										xlab=L"\mbox{Season}", 
+										ylab=L"\mbox{Number of teams eliminated}",
+										legend=:best,
+										legendfont=6,
+										legendtitle=L"\mbox{Draft ranking breakpoint}",
+										titlefont=12,
+										tickfont=8,
+										grid=false,
+										show=false)
+		savefig(fig, string(results_dir,"/nba_num_teams_eliminated",ext));
+	end
 
 	return
 end # main_parse
