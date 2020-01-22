f0b81df37e64fbed4c23807526a8ffedd1d541aa
Aleksandr Kazachkov <>
2020-01-22 16:28:55 -0500

    Updated model validation to work again.

--- julia/main.jl
+++ julia/main.jl
@@ -705,7 +705,7 @@ function main_parse(;do_plotting=true, mode=MODE, data_dir="../data", results_di
 											ylab=ylabelstring,
 											legendtitle=legendtitlestring,
 											#xticks=(Array(1:num_years),["\$13-14\$","\$14-15\$","\$15-16\$","\$16-17\$","\$17-18\$"]),
-											xticks=(Array(1:num_years),labels)
+											xticks=(Array(1:num_years),labels),
 											yticks=(Array(miny:incy:maxy),[@sprintf("\$%d\$", i) for i in miny:incy:maxy]),
 											group=ctg,
 											lw=0,
@@ -954,11 +954,11 @@ function model_validation(;do_simulation = true, num_replications = 100000,
 
   ## Simulation parameters
   #mode_list = [BT_ESTIMATED BT_DISTR]; mode_list_name = ["BT.est", "BT.beta"]
-  #mode_list = [BT_ESTIMATED]; mode_list_name= ["BT"]
-  mode_list = []; mode_list_name = []
+  mode_list = [BT_ESTIMATED]; mode_list_name= ["BT"]
+  #mode_list = []; mode_list_name = []
   @assert(length(mode_list) == length(mode_list_name))
   #gamma_list = [0.50 0.55 0.60 0.65 0.70 0.7125 0.725 0.7375 0.75 0.80 0.85 0.90 0.95 1.00]
-  gamma_list = [0.71425, 0.7115]
+  gamma_list = [0.7, 0.715, 0.725]
   num_modes = length(mode_list) + length(gamma_list)
 
   ## Stats we keep
@@ -1003,15 +1003,16 @@ function model_validation(;do_simulation = true, num_replications = 100000,
         writedlm(string(results_dir, "/", prefix[stat], curr_name, csvext), win_pct[:,:,stat], ',')
       end
     else
+      win_pct = zeros(Float64, num_steps+1, num_teams, num_stats)
       for stat in [avg_stat]
         curr_name = "win_pct"
         if curr_mode == STRICT
           curr_name = string(curr_name, "_", curr_gamma)
         end
-        win_pct_avg = readdlm(string(results_dir, "/", prefix[stat], curr_name, csvext), ',')
-        win_pct_list[mode_ind, :, :, avg_stat] = win_pct_avg
+        win_pct[:,:,stat] = readdlm(string(results_dir, "/", prefix[stat], curr_name, csvext), ',')
+        win_pct_list[mode_ind, :, :, stat] = win_pct[:,:,stat]
       end
-    end
+    end # do_simulation
 
     ## Calculate mean squared error
     for step_ind = 1:num_steps+1
