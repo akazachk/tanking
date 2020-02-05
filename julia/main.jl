a68098df8972124600397ced74e01f823950375d
Aleksandr Kazachkov <None>
2020-02-05 14:26:13 -0500

    Added results for gamma=.72 and gamma=.75 and noisy_ranking_strict from rankings_are_noisy(do_simulation=true,num_replications=10000, do_plotting=false)

--- julia/main.jl
+++ julia/main.jl
@@ -1021,10 +1021,10 @@ function model_validation(;do_simulation = true, num_replications = 100000,
   ## Simulation parameters
   #mode_list = [BT_ESTIMATED BT_DISTR]; mode_list_name = ["BT.est", "BT.beta"]
   mode_list = [BT_ESTIMATED]; mode_list_name= ["BT"]
-  #mode_list = []; mode_list_name = []
+  mode_list = []; mode_list_name = []
   @assert(length(mode_list) == length(mode_list_name))
   #gamma_list = [0.50 0.55 0.60 0.65 0.70 0.7125 0.725 0.7375 0.75 0.80 0.85 0.90 0.95 1.00]
-  gamma_list = [0.7, 0.715, 0.725]
+  gamma_list = [0.72, 0.75]
   num_modes = length(mode_list) + length(gamma_list)
 
   ## Stats we keep
